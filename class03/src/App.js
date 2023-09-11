2d04caec3ee9df4eb1fd0f52ea26d9e3309a25b3
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-09-11 17:54:15 +0900

    Add files via upload

---
+++ class03/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Variable from './Variable';
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <Variable />
+    </div>
+  );
+}
+
+export default App;
